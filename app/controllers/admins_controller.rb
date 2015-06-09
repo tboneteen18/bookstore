812588fb49501c6431b20cedbeddceb694492ad9
tboneteen18 <tboneteen18@gmail.com>
2015-06-09 15:11:06 -0600

    added things

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,3 @@
+class AdminsController < ActionController::Base
+
+end
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,9 @@
+class BooksController < ActionController::Base
+  def index
+      @books = Book.all
+  end
+
+  def show
+      @book = Book.find(params[:id])
+  end
+end
