8c99e92569c4cabe814f4c3551fdb0ee75bb099a
stephyc93 <stephyc93@gmail.com>
2015-06-09 16:07:15 -0600

    added new book button

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ActionController::Base
+  # before_action :authenticate_admin
+  before_action :find_book, only: [:edit, :update, :show, :destroy]
+
   def index
       @books = Book.all
   end
@@ -6,4 +9,48 @@ class BooksController < ActionController::Base
   def show
       @book = Book.find(params[:id])
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def new
+    @book = Book.new
+
+  end
+
+  def show
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre)
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
 end
